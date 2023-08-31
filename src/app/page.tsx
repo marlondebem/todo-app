e078878316c3c5d74fe35c21b7ecc2b26ae13184
Marlon de Bem da Silva <marlon.bem@useall.inf.br>
2023-08-31 11:39:05 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 import { TaskForm } from '@/components/TaskForm'
 import { TaskList } from '@/components/TaskList'
