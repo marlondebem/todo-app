51677b4c03cd9a757151e5c2976fd3da1ae5a3cd
marlonjs <marlondbem@icloud.com>
2023-08-31 22:22:29 -0300

    fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 import { TaskForm } from '@/components/TaskForm'
 import { TaskList } from '@/components/TaskList'
