6c787e2c369f3459cdcb47539a8b815e11938787
marlonjs <marlondbem@icloud.com>
2023-09-05 20:48:05 -0300

    feat: Responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { DarkModeButton } from './DarkModeButton'
 export function Header() {
   return (
     <header className='sticky top-0 w-full h-14 flex items-center justify-center bg-zinc-100/30 dark:bg-zinc-950/30 backdrop-filter backdrop-blur border-b dark:border-b-zinc-900'>
-      <nav className='max-w-4xl w-full flex items-center justify-between'>
+      <nav className='max-w-sm px-4 md:max-w-3xl lg:max-w-4xl w-full flex items-center justify-between'>
         <span className='font-semibold text-base text-zinc-600 dark:text-zinc-200 select-none'>Tasks</span>
         <DarkModeButton />
       </nav>
